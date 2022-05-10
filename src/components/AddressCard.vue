7eb870d6caac3b372063e1e70474521dd7004c32
Alireza Kasir <alirezakasir@yahoo.com>
2022-05-10 17:35:27 +0430

    create address card component

---
+++ src/components/AddressCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card overflow-hidden">
+    <h5 class="card-header">آدرس ۱</h5>
+    <div class="card-body text-center">
+      <h5 class="card-title">علیرضا کثیرزارع</h5>
+      <p class="card-text mb-0">123345345</p>
+    </div>
+    <div>
+      <button class="btn btn-primary d-block w-100 rounded-0">
+        اطلاعات بیشتر
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// import { defineProps } from 'vue';
+// props
+// const props = defineProps(['item']);
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,18 @@
 <template>
   <!-- title -->
-  <h4>لیست آدرس ها</h4>
+  <h5 class="mb-4">لیست آدرس ها</h5>
   <!-- address lists -->
-  <!-- <div class="row g-3 justify-content-center">
-    <div class="col-12 col-md-6 col-lg-4" v-for="n in 0" :key="n">
-      <div class="card overflow-hidden">
-        <h5 class="card-header">آدرس ۱</h5>
-        <div class="card-body text-center">
-          <h5 class="card-title">علیرضا کثیرزارع</h5>
-          <p class="card-text mb-0">123345345</p>
-        </div>
-        <div>
-          <button class="btn btn-primary d-block w-100 rounded-0">
-            اطلاعات بیشتر
-          </button>
-        </div>
-      </div>
+  <div class="row g-3 justify-content-center" v-if="itemsAddress.length">
+    <div
+      class="col-12 col-md-6 col-lg-4"
+      v-for="item in itemsAddress"
+      :key="item.id"
+    >
+      <AddressCard :item="item" />
     </div>
-  </div> -->
+  </div>
   <!-- not found item -->
-  <dir class="p-0 pt-5">
+  <dir class="p-0 pt-5" v-else>
     <ErrorPage
       text="هیچ آدرسی یافت نشد"
       image-url="no-item.svg"
@@ -30,5 +23,24 @@
 </template>
 
 <script setup>
+import { ref } from '@vue/reactivity';
+// components
 import ErrorPage from '@/components/ErrorPage.vue';
+import AddressCard from '@/components/AddressCard.vue';
+
+// address items
+const itemsAddress = ref([
+  // {
+  //   id: '1234',
+  //   text: 'slam 1',
+  // },
+  // {
+  //   id: '123432',
+  //   text: 'slam 2',
+  // },
+  // {
+  //   id: '1234234',
+  //   text: 'slam 3',
+  // },
+]);
 </script>
