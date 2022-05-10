c2e152065e68b3f093e7f4d45210edea21d071cf
Alireza Kasir <alirezakasir@yahoo.com>
2022-05-10 17:25:29 +0430

    create error page component

---
+++ src/components/ErrorPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- error image -->
+  <img
+    :src="require(`@/assets/images/${props.imageUrl}`)"
+    alt="no item founded"
+    class="no-item-image"
+  />
+
+  <!-- error text -->
+  <p class="mt-5 text-center text-secondary">{{ props.text }}</p>
+
+  <!-- target link -->
+  <div class="d-flex justify-content-center">
+    <router-link :to="props.linkTarget" class="btn btn-primary">{{
+      props.linkText
+    }}</router-link>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+// props
+const props = defineProps(['text', 'image-url', 'link-text', 'link-target']);
+</script>
+
+<style lang="scss" scoped>
+.no-item-image {
+  width: 50%;
+  min-width: 300px;
+  display: block;
+  margin: auto;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,51 +1,34 @@
 <template>
-  <div class="home">
-    <!-- address lists -->
-    <div class="row g-3 justify-content-center">
-      <div class="col-12 col-md-6 col-lg-4" v-for="n in 0" :key="n">
-        <div class="card overflow-hidden">
-          <h5 class="card-header">آدرس ۱</h5>
-          <div class="card-body text-center">
-            <h5 class="card-title">علیرضا کثیرزارع</h5>
-            <p class="card-text mb-0">123345345</p>
-          </div>
-          <div>
-            <button class="btn btn-primary d-block w-100 rounded-0">
-              اطلاعات بیشتر
-            </button>
-          </div>
+  <!-- title -->
+  <h4>لیست آدرس ها</h4>
+  <!-- address lists -->
+  <!-- <div class="row g-3 justify-content-center">
+    <div class="col-12 col-md-6 col-lg-4" v-for="n in 0" :key="n">
+      <div class="card overflow-hidden">
+        <h5 class="card-header">آدرس ۱</h5>
+        <div class="card-body text-center">
+          <h5 class="card-title">علیرضا کثیرزارع</h5>
+          <p class="card-text mb-0">123345345</p>
+        </div>
+        <div>
+          <button class="btn btn-primary d-block w-100 rounded-0">
+            اطلاعات بیشتر
+          </button>
         </div>
       </div>
     </div>
-    <!-- not found item -->
-    <dir class="p-0 pt-5">
-      <img
-        src="@/assets/images/no-item.svg"
-        alt="no item founded"
-        class="no-item-image"
-      />
-
-      <p class="mt-5 text-center text-secondary">آدرسی پیدا نشد</p>
-      <div class="d-flex justify-content-center">
-        <router-link to="/add" class="btn btn-primary"
-          >اضافه کردن آدرس</router-link
-        >
-      </div>
-    </dir>
-  </div>
+  </div> -->
+  <!-- not found item -->
+  <dir class="p-0 pt-5">
+    <ErrorPage
+      text="هیچ آدرسی یافت نشد"
+      image-url="no-item.svg"
+      linkTarget="/add"
+      linkText="اضافه کردن یک آدرس"
+    />
+  </dir>
 </template>
 
-<script>
-export default {
-  name: 'HomeView',
-};
+<script setup>
+import ErrorPage from '@/components/ErrorPage.vue';
 </script>
-
-<style lang="scss" scoped>
-.no-item-image {
-  width: 50%;
-  min-width: 300px;
-  display: block;
-  margin: auto;
-}
-</style>
