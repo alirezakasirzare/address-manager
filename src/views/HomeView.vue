e5a6d537602d49a259a9817931056922a3f4aa44
Alireza Kasir <alirezakasir@yahoo.com>
2022-05-10 16:12:22 +0430

    add home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,18 @@
 <template>
   <div class="home">
     <!-- address lists -->
-    <div class="row g-2">
-      <div class="col-12 col-md-6">
+    <div class="row g-3 justify-content-center">
+      <div class="col-12 col-md-6 col-lg-4" v-for="n in 19" :key="n">
         <div class="card overflow-hidden">
           <h5 class="card-header">آدرس ۱</h5>
           <div class="card-body text-center">
             <h5 class="card-title">علیرضا کثیرزارع</h5>
-            <p class="card-text">123345345</p>
+            <p class="card-text mb-0">123345345</p>
           </div>
           <div>
-            <a href="#" class="btn btn-primary d-block rounded-0"
-              >اطلاعات بیشتر</a
-            >
-          </div>
-        </div>
-      </div>
-
-      <div class="col-12 col-md-6">
-        <div class="card overflow-hidden">
-          <h5 class="card-header">آدرس ۲</h5>
-          <div class="card-body text-center">
-            <h5 class="card-title">علی بهادری</h5>
-            <p class="card-text">123345345</p>
-          </div>
-          <div>
-            <a href="#" class="btn btn-primary d-block rounded-0"
-              >اطلاعات بیشتر</a
-            >
+            <button class="btn btn-primary d-block w-100 rounded-0">
+              اطلاعات بیشتر
+            </button>
           </div>
         </div>
       </div>
