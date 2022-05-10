d7534a83b4bc3fc0a3543792ca66992e43b635be
Alireza Kasir <alirezakasir@yahoo.com>
2022-05-10 14:03:56 +0430

    add cards for home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="home">hello world</div>
+  <div class="home">
+    <!-- address lists -->
+    <div class="row g-2">
+      <div class="col-12 col-md-6">
+        <div class="card overflow-hidden">
+          <h5 class="card-header">it is address 1</h5>
+          <div class="card-body text-center">
+            <h5 class="card-title">Alireza Kasirzare</h5>
+            <p class="card-text">123345345</p>
+          </div>
+          <div>
+            <a href="#" class="btn btn-primary d-block rounded-0">see more</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-6">
+        <div class="card overflow-hidden">
+          <h5 class="card-header">it is address 2</h5>
+          <div class="card-body text-center">
+            <h5 class="card-title">Reza Loremi</h5>
+            <p class="card-text">123345345</p>
+          </div>
+          <div>
+            <a href="#" class="btn btn-primary d-block rounded-0">see more</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
